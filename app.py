9a5b4a456a5f9003fb8e0e8442ad572db00ff59f
Sandeep Singh Shekhawat <103978330+Sandeep9828@users.noreply.github.com>
2023-05-08 14:24:53 +0530

    Add files via upload
    This project is a spam classifier that uses NLTK (Natural Language Toolkit) and Naive Bayes algorithm to classify emails and SMS as spam or not. The dataset used for training and testing the model was preprocessed by performing data cleaning and visualizations. The model was deployed using Streamlit library to convert it into a website form.
    
    The code for this project is available on GitHub, and it can be used to classify emails and SMS as spam or not. The project is useful for people who want to automate the process of identifying spam emails and SMS. Additionally, the code can be modified and improved to make it more accurate and efficient.
    
    Overall, this project demonstrates how to use NLTK, Naive Bayes algorithm, data cleaning, visualizations, and Streamlit library to create a spam classifier.

---
+++ app.py
@@ -0,0 +1,66 @@
+import pickle
+import streamlit as st
+import string
+import nltk
+from nltk.corpus import stopwords
+from nltk.stem import PorterStemmer
+
+ps = PorterStemmer()
+
+
+def preprocess(text):
+    text = text.lower()
+
+    # Tokenization
+    text = nltk.word_tokenize(text)
+
+    # special character
+    y = []
+    for i in text:
+        if i.isalnum():
+            y.append(i)
+
+    text = y[:]
+    y.clear()
+
+    # Remove stopwords
+    stops = stopwords.words('english')
+    for i in text:
+        if i not in stops and i not in string.punctuation:
+            y.append(i)
+
+    # Stemming
+    text = y[:]
+    y.clear()
+
+    for i in text:
+        y.append(ps.stem(i))
+
+    return " ".join(y)
+
+
+# Steps
+tfidf = pickle.load(open('vectorizer.pkl', 'rb'))
+model = pickle.load(open('model.pkl', 'rb'))
+
+st.title("Email/SMS Spam Classifier")
+input_sms = st.text_area("Enter the Message")
+if st.button('Predict'):
+    # 1. Preprocess
+
+    transformed_sms = preprocess(input_sms)
+
+    # 2. Vectorize
+
+    vector_input = tfidf.transform([transformed_sms])
+
+    # 3. predict
+
+    result = model.predict(vector_input)[0]
+
+    # 4. Display
+
+    if result == 1:
+        st.header("Spam")
+    else:
+        st.header("Not Spam")
